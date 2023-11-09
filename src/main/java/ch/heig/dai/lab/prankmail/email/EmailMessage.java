c4c6d816e35bb29971f8da8e335a73e2daea686d
LucasHSN <lucas.hussain@heig-vd.ch>
2023-11-09 11:22:16 +0100

    Add comments

--- src/main/java/ch/heig/dai/lab/prankmail/email/EmailMessage.java
+++ src/main/java/ch/heig/dai/lab/prankmail/email/EmailMessage.java
@@ -2,6 +2,9 @@ package main.java.ch.heig.dai.lab.prankmail.email;
 
 import java.util.*;
 
+/**
+ * EmailMessage class allows to forge an email message with the given parameters
+ */
 public class EmailMessage {
     private String from;
     private String to;
@@ -9,6 +12,13 @@ public class EmailMessage {
     private String subject;
     private String content;
 
+    /**
+     * Constructor
+     * @param from Sender email address
+     * @param to Recipient email address
+     * @param subject Subject of the email
+     * @param content Data of the email
+     */
     public EmailMessage(String from, String to, String subject, String content) {
         this.from = "From: " + getName(from) + " <" + from + ">";
         this.to = "To: <" + to + ">";
@@ -17,16 +27,33 @@ public class EmailMessage {
         this.date = "Date: " + new Date().toString();
     }
 
+    /**
+     * Constructor
+     * @param from Sender email address
+     * @param to Recipient email address
+     * @param subject Subject of the email
+     * @param content Data of the email
+     * @param date Date of the email
+     */
     public EmailMessage(String from, String to, String subject, String content, String date) {
         this(from, to, subject, content);
         this.date = "Date: " + date;
     }
 
+    /**
+     * Get the name of the sender
+     * @param email Email address of the sender
+     * @return Name of the sender
+     */
     private String getName(String email) {
         String name = email.split("@")[0];
         return name.substring(0, 1).toUpperCase() + name.substring(1);
     }
 
+    /**
+     * Forge the email message
+     * @return Email message
+     */
     public String forge() {
         return from + "\n" + to + "\n" + date + "\n" + subject + "\n\n" + content + "\n\n.\n";
     }
