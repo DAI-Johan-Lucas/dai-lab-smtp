8e65f84b0fb2deee733fc8b833b6a2476337cce3
LucasHSN <lucas.hussain@heig-vd.ch>
2023-11-09 11:00:58 +0100

    Add feature read file

--- src/main/java/ch/heig/dai/lab/prankmail/config/ConfigReader.java
+++ src/main/java/ch/heig/dai/lab/prankmail/config/ConfigReader.java
@@ -7,4 +7,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConfigReader {
+    public static List<String> readLines(String filePath) throws IOException {
+        List<String> lines = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+
+            while ((line = reader.readLine()) != null)
+                lines.add(line);
+        }
+
+        return lines;
+    }
 }
