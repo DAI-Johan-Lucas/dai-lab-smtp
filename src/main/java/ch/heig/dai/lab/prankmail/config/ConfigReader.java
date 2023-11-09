8812a10e8febf8386dc83d15d5c51b76696a049e
LucasHSN <lucas.hussain@heig-vd.ch>
2023-11-09 11:02:29 +0100

    Add comments

--- src/main/java/ch/heig/dai/lab/prankmail/config/ConfigReader.java
+++ src/main/java/ch/heig/dai/lab/prankmail/config/ConfigReader.java
@@ -6,7 +6,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * ConfigReader class allows to read a file line by line
+ */
 public class ConfigReader {
+
+    /**
+     * Read a file line by line
+     * @param filePath the path of the file to read
+     * @return a list of lines
+     * @throws IOException if the file doesn't exist
+     */
     public static List<String> readLines(String filePath) throws IOException {
         List<String> lines = new ArrayList<>();
 
