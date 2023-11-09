b1fbea57ba2f121a6c6e24ebe52e9bc294f431cb
LucasHSN <lucas.hussain@heig-vd.ch>
2023-11-09 13:45:27 +0100

    Complete main

--- src/main/java/ch/heig/dai/lab/prankmail/Main.java
+++ src/main/java/ch/heig/dai/lab/prankmail/Main.java
@@ -5,9 +5,18 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            System.out.println("Usage: Main <emailFile> <messageFile> <numberOfGroups>");
+            return;
+        }
+
+        /// TODO je sais pas si le premier arguments = args[0] ou args[1]
         Controller ctrl = new Controller(args[0], args[1]);
-        ctrl.generatePrankGroups(Integer.parseInt(args[2]));
 
-
+        ctrl.sendPrankEmails(
+                ctrl.generatePrankGroups(Integer.parseInt(args[2])),
+                "127.0.0.1",
+                25
+        );
     }
 }
