cd35059af467b389f1c9248d55b35e84d8c01321
LucasHSN <lucas.hussain@heig-vd.ch>
2023-11-24 09:08:37 +0100

    Fix messagesFilePath in Tests

--- src/test/java/prankmail/SMTPClientTest.java
+++ src/test/java/prankmail/SMTPClientTest.java
@@ -13,7 +13,8 @@ public class SMTPClientTest {
 //                    "\\labo\\dai-lab-smtp\\src\\test\\java\\ch\\heig\\dai\\lab" +
 //                    "\\prankmail\\test_email.txt", "C:\\Users\\Mikami\\Documents\\DAI\\labo\\dai-lab-smtp\\src\\test\\java\\ch\\heig\\dai\\lab\\prankmail\\test_message.txt");
 
-            Controller ctrl = new Controller("src/test/java/prankmail/test_email.txt", "src/test/java/prankmail/test_message.t");
+            Controller ctrl = new Controller("src/test/java/prankmail/test_email.txt",
+                    "src/test/java/prankmail/test_message.txt");
 
             ctrl.sendPrankEmails(
                     ctrl.generatePrankGroups(2),
